e2c32247db6bfccf1de024e37284b577d58c3b14
Rhnbmpl <rb13@iitbbs.ac.in>
2016-06-10 13:29:49 +0530

    Output showing properly
    Output was printing in byte form, so I added decode('ascii') to convert
    the output to ascii form.

--- Hotspot.py
+++ Hotspot.py
@@ -5,15 +5,15 @@ stri='netsh wlan set hostednetwork mode=allow ssid=%s key=%s'%(user,key)
 net=subprocess.Popen(stri,shell=False,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
 output,errors=net.communicate()
 if errors:
-    print('WARNING : ',errors)
+    print('WARNING : ',errors.decode('ascii'))
 elif output:
-    print('SUCCESS :',output)
+    print('SUCCESS :',output.decode('ascii'))
     sta=subprocess.Popen('netsh wlan start hostednetwork',shell=False,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
     out,err=sta.communicate()
     if err:
-        print('ERR START : ',err)
+        print('ERR START : ',err.decode('ascii'))
     elif out:
-        print('STARTED : ',out)
+        print('STARTED : ',out.decode('ascii'))
 
 
 
